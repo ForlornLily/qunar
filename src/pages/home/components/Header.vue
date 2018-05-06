5760936010bbfb631a36c892ac89d8bda0c5016b
bareisho <tudoutasty@gmail.com>
2018-05-06 16:55:16 +0800

    猜你喜欢部分

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,7 +8,7 @@
       输入城市/景点/游玩主题
     </div>
     <div class="header-right">
-      城市<span class="iconfont arrow-icon">&#xe64a;</span>
+      上海<span class="iconfont arrow-icon">&#xe64a;</span>
     </div>
   </div>
 </template>
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,33 +1,96 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-      </div>
-      <p class="icon-intro">123456</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-      </div>
-      <p class="icon-intro">123</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of page" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-intro">{{item.title}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: false
+      },
+      iconList:[{
+        id: 1,
+        title: "热门景点",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 2,
+        title: "热门景点2",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 3,
+        title: "热门景点3",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      },{
+        id: 4,
+        title: "热门景点",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 5,
+        title: "热门景点2",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 6,
+        title: "热门景点3",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      },{
+        id: 7,
+        title: "热门景点",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 8,
+        title: "热门景点2",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }, {
+        id: 9,
+        title: "热门景点3",
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+      }]
+    }
+  },
+  computed: {
+    //计算swiper-slide生成个数
+    page () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
 @import "~style/varibles.styl"
-  .icons
+@import "~style/mixcommon.styl"
+/*  ellipsis() 通用的一组样式*/
+/* swiper高度为div.icon高度，而没有撑满整个div.icons，
+需要进行处理，使其撑满 */
+ /*  .icons
+   height:0
+   padding-bottom: 50%
+   overflow: hidden */
+   .icons >>> .swiper-container
     height:0
     padding-bottom: 50%
-    overflow: hidden
     .icon
       width: 25%
       float: left
@@ -56,4 +119,5 @@ export default {
           line-height: .44rem
           text-align: center
           color: $darkTextColor
+          ellipsis()
 </style>
